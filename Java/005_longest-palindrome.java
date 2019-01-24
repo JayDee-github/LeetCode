853982b0f2b577083f80ea16f8fcc512b30d93e8
JD <jayday7070@gmail.com>
2019-01-23 19:51:05 -0500

    Storing indices rather than substrings
    Updating the algorithm to store detected palindromes as indices rather
    than strings.
    
    CHANGES:
    * Added a class to hold string indices and length.
    * Improved efficiency by creating the final string at the end instead of
    every loop iteration.

--- Java/005_longest-palindrome.java
+++ Java/005_longest-palindrome.java
@@ -1,44 +1,62 @@
 class Solution {
-    public String longestPalindrome(String s) {
-      if (s == null || s.length() == 0)
-        return "";
-      
-      String maxPali = "";
-      int maxLength = 0;
-      
-      for (int i=0; i<s.length(); i++) {
-        String strOdd = getPali(s, i, i);
-        String strEven = getPali(s, i, i + 1);
-       
-        String strMax = strOdd.length() > strEven.length() ?
-          strOdd : strEven;
-        
-        if (strMax.length() > maxLength) {
-          maxPali = strMax;
-          maxLength = strMax.length();
-        }
-      }
-      
-      return maxPali;
+  class StringIndices {
+    public int start;
+    public int end;
+    public int length;
+
+    public StringIndices(int s, int e) {
+      this.start = s;
+      this.end = e;
+      this.length = end - start;
     }
-  
-    public String getPali(String str, int l, int r) {
-      if ((str == null) ||
-          (str.length() == 0) ||
-          (l < 0) ||
-          (r >= str.length()))
-        return "";
-      
-      int left = l;
-      int right = r;
-      
-      while ((left >= 0) &&
-             (right < str.length()) &&
-             (str.charAt(left) == str.charAt(right))) {
-        left--;
-        right++;
-      }
-      
-      return str.substring(left + 1, right);
+  }
+
+  public String longestPalindrome(String s) {
+    if (s == null || s.length() == 0)
+      return "";
+
+    StringIndices maxPali = new StringIndices(0, 0);
+
+    for (int i=0; i<s.length(); i++) {
+      StringIndices oddIndices = getPali(s, i, i);
+      StringIndices evenIndices = getPali(s, i, i + 1);
+
+      StringIndices strMax;
+
+      if ((oddIndices == null) && (evenIndices == null))
+        continue;
+      else if (evenIndices == null)
+        strMax = oddIndices;
+      else if (oddIndices == null)
+        strMax = evenIndices;
+      else
+        strMax = oddIndices.length > evenIndices.length ?
+          oddIndices : evenIndices;
+
+      if (strMax.length > maxPali.length)
+        maxPali = strMax;
     }
+
+    return s.substring(maxPali.start, maxPali.end);
+  }
+
+  public StringIndices getPali(String str, int l, int r) {
+    if ((str == null) ||
+        (str.length() == 0) ||
+        (l < 0) ||
+        (r >= str.length()))
+      return null;
+
+    int left = l;
+    int right = r;
+
+    while ((left >= 0) &&
+           (right < str.length()) &&
+           (str.charAt(left) == str.charAt(right))) {
+      left--;
+      right++;
+    }
+
+    return new StringIndices(left + 1, right);
+  }
 }
